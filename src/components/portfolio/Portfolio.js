c74debe7d735abc553beaceaed38c226a864f4fa
Ariel_Rodriguez <32606708+Ariels713@users.noreply.github.com>
2020-10-19 22:51:13 -0400

    changes to portfolio modol

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 // reactstrap components
 import { Button, FormGroup, Input, Modal, Form } from "reactstrap";
 
 function Example() {
-  const [loginModal, setLoginModal] = React.useState(false);
+  const [loginModal, setLoginModal] = useState(false);
+  const [createUser, setCreateUser] = useState(false);
+  const [logIn, setLogIn] = useState({
+    register: true,
+    user: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const buttonHandler = (e) => {
+    e.preventDefault();
+    setCreateUser(!createUser);
+  };
+
   return (
     <>
       <Button
@@ -29,33 +43,92 @@ function Example() {
           >
             <span aria-hidden={true}>×</span>
           </button>
-          <h3 className="modal-title text-center">Portfolio</h3>
-          <p>Log in to see my portfolio</p>
+          {/* <h3 className="modal-title text-center">Portfolio</h3> */}
+          {createUser ? (
+            <>
+              <h3 className="modal-title text-center">Portfolio</h3>
+              <p>Log in to see my portfolio</p>
+            </>
+          ) : (
+            <>
+              <h3 className="modal-title text-center">Register</h3>
+              <p>Register to see my portfolio</p>
+            </>
+          )}
         </div>
         <div className="modal-body">
-          <Form>
-            <FormGroup>
-              <label>Email</label>
-              <Input defaultValue="" placeholder="Email" type="text" />
-            </FormGroup>
-            <FormGroup>
-              <label>Password</label>
-              <Input defaultValue="" placeholder="Password" type="password" />
-            </FormGroup>
-            <Button block className="btn-round" color="success">
-              Log in
-            </Button>
-          </Form>
+          {createUser ? (
+            <Form>
+              <FormGroup>
+                <label>Email</label>
+                <Input defaultValue="" placeholder="Email" type="text" />
+              </FormGroup>
+              <FormGroup>
+                <label>Password</label>
+                <Input defaultValue="" placeholder="Password" type="password" />
+              </FormGroup>
+              <Button block className="btn-round" color="success">
+                Log in
+              </Button>
+            </Form>
+          ) : (
+            <>
+              <div className="social text-center">
+                <Button className="btn-just-icon mr-1" color="facebook">
+                  <i className="fa fa-facebook" />
+                </Button>
+                <Button className="btn-just-icon mr-1" color="google">
+                  <i className="fa fa-google" />
+                </Button>
+                <Button className="btn-just-icon" color="twitter">
+                  <i className="fa fa-twitter" />
+                </Button>
+              </div>
+              <Form>
+                <FormGroup>
+                  <label>Name</label>
+                  <Input defaultValue="" placeholder="Name" type="text" />
+                </FormGroup>
+                <FormGroup>
+                  <label>Email</label>
+                  <Input defaultValue="" placeholder="Email" type="text" />
+                </FormGroup>
+                <FormGroup>
+                  <label>Password</label>
+                  <Input
+                    defaultValue=""
+                    placeholder="Password"
+                    type="password"
+                  />
+                </FormGroup>
+                <Button block className="btn-round" color="success">
+                  Register
+                </Button>
+              </Form>
+            </>
+          )}
         </div>
-        {/* <div className="modal-footer no-border-footer">
-          <span className="text-muted text-center">
-            Looking{" "}
-            <a href="#pablo" onClick={(e) => e.preventDefault()}>
-              create an account
-            </a>
-            ?
-          </span>
-        </div> */}
+        {createUser ? (
+          <div className="modal-footer no-border-footer">
+            <span className="text-muted text-center">
+              Looking{" "}
+              <a href="#pablo" onClick={buttonHandler}>
+                create an account
+              </a>
+              ?
+            </span>
+          </div>
+        ) : (
+          <div className="modal-footer no-border-footer">
+            <span className="text-muted text-center">
+              Registered?{" "}
+              <a href="#pablo" onClick={buttonHandler}>
+                Log In
+              </a>
+              ?
+            </span>
+          </div>
+        )}
       </Modal>
     </>
   );
